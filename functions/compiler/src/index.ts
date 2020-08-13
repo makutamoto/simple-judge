35853fb107a85cd87cf288562d4a11cdfffebc3d
Makutamoto <34860673+makutamoto@users.noreply.github.com>
2020-08-14 01:19:20 +0900

    !

--- functions/compiler/src/index.ts
+++ functions/compiler/src/index.ts
@@ -3,7 +3,7 @@ export const handler = async (event: any) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'successfully',
+        message: 'successfully!',
         input: event,
       },
       null,
